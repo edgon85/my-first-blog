49386782c989344fccaf2df40b73f133fa4415fd
Edgon <edgon85@gmail.com>
2016-02-21 18:37:35 -0600

    Mi app django, primer Commit

---
+++ myvemv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\edgon\desktop\programacion web\django\djangogirls\myvemv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
