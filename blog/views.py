49a145c469f4b69b4b3c185055c4b50cd0ae33df
Edgon <edgon85@gmail.com>
2016-02-26 11:15:11 -0600

    Plantillas modificadas para mostrar mensajes desde la base de datos

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,12 @@
-import datetime
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 
-# def post_list(request):
-#     return render(request, 'blog/post_list.html', {})
-
 def post_list(request):
-	return render(request, 'blog/post_list.html', {})
+	# posts = Post.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('published_date')
+	posts = Post.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
+	return render(request, 'blog/post_list.html', {'posts':posts})
+
+
