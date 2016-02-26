8370b6cab5832d71eb4844d000888dad01641d0e
Edgon <edgon85@gmail.com>
2016-02-25 23:42:34 -0600

    Cambie el html para la página, se crearon los templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
+import datetime
 from django.shortcuts import render
 
 # Create your views here.
+
+# def post_list(request):
+#     return render(request, 'blog/post_list.html', {})
+
+def post_list(request):
+	return render(request, 'blog/post_list.html', {})
